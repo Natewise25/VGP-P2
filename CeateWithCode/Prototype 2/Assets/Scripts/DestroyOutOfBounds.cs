0393e044ff5ef719a9d425fdda6d6f0750736352
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2022-10-12 09:28:15 -0600

    fIXED ERRORS

--- CeateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ CeateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOutOfBounds : MonoBehaviour
-private float topBound = 30;
+  public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float topBound = 30;
     // Start is called before the first frame update
     void Start()
     {
