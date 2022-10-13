e9c96bdc0a3aface803bf190799983b2dc6001fe
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2022-10-13 09:27:48 -0600

    Added prefabs

--- CeateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ CeateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-  public class DestroyOutOfBounds : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
-  private float topBound = 20;
+    public GameObject[] animalPrefabs;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +14,6 @@ using UnityEngine;
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound)
-        {
-           Destroy(gameObject);
-        }
+        
     }
 }
