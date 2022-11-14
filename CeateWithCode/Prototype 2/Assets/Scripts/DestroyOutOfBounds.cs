5424fba34224a0494c254246c6ed30a11a4a2eb5
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2022-11-14 09:21:27 -0700

    Deleted ball

--- CeateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ CeateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
   public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float leftLimit = -30;
+    private float leftLimit = 30;
     private float bottomLimit = -5;
     // Start is called before the first frame update
     void Start()
