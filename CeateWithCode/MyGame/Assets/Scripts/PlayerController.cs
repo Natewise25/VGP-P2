d5cded7741258a192461b18bb2895c4d8bc9d6b9
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2023-02-10 10:10:37 -0700

    worked on lab

--- CeateWithCode/MyGame/Assets/Scripts/PlayerController.cs
+++ CeateWithCode/MyGame/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
+    private float zBound = 6;
     private Rigidbody playerRb;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +22,17 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        
     }
+    // Moves the player based on arrow key input
 }
