12e604f31509aa456dbb9b2b87418ba5583cd384
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2023-03-10 09:11:48 -0800

    Added Score

--- CeateWithCode/Prototype5/Assets/Scripts/GameManager.cs
+++ CeateWithCode/Prototype5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f; 
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+       
     }
 
     // Update is called once per frame
@@ -22,10 +29,18 @@ public class GameManager : MonoBehaviour
     {
         while(true)
         {
-         yield return new WaitForSeconds(spawnRate);
-        int index = Random.Range(0, targets.Count);
-        Instantiate(targets[index]);
+             yield return new WaitForSeconds(spawnRate);
+             int index = Random.Range(0, targets.Count);
+             Instantiate(targets[index]);
+
+            UpdateScore(5);
         }
         
+    }
+    
+     public void UpdateScore(int scoreToAdd)
+    {
+         score += scoreToAdd;
+       scoreText.text = "Score: " + score;
     }
 }
