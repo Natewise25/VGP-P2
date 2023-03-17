150ec3e8a21d90943cf112ee1248241a8e193ca9
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2023-03-17 09:11:15 -0700

    added menu buittons

--- CeateWithCode/Prototype5/Assets/Scripts/GameManager.cs
+++ CeateWithCode/Prototype5/Assets/Scripts/GameManager.cs
@@ -7,23 +7,23 @@ using UnityEngine.UI;
 
 
 
+
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f; 
     
+    
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -59,5 +59,15 @@ public class GameManager : MonoBehaviour
     {
         restartButton.gameObject.SetActive(true);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame()
+    {
+        isGameActive = true;
+        score = 0;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
     }
 }
