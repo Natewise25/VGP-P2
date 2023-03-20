fce1d3d63ae30a91979d0ed77aba5b775f50f3ae
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2023-03-20 09:02:14 -0700

    Added difficulty buttons

--- CeateWithCode/Prototype5/Assets/Scripts/DifficultyButton.cs
+++ CeateWithCode/Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -7,6 +7,7 @@ public class DifficultyButton : MonoBehaviour
 {
     private Button button;
     private GameManager gameManager;
+    public int difficulty;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +25,7 @@ public class DifficultyButton : MonoBehaviour
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + "was clicked");
-        gameManager.StartGame();
+        gameManager.StartGame(difficulty);
     }
     
 }
