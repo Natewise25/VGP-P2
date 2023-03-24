d2d7dce99dbcf409a39a247b3a0efa2d4afa9d6d
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2023-03-24 09:10:42 -0700

    added pause screen

--- CeateWithCode/Prototype5/Assets/Scripts/GameManager.cs
+++ CeateWithCode/Prototype5/Assets/Scripts/GameManager.cs
@@ -20,7 +20,8 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f; 
     public TextMeshProUGUI livesText;
     private int lives;
-    
+    public GameObject pauseScreen;
+    private bool paused;
     
     // Start is called before the first frame update
     void Start()
@@ -31,7 +32,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if the user has pressed the P key
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
     IEnumerator SpawnTarget()
     {
@@ -83,5 +88,21 @@ public class GameManager : MonoBehaviour
         UpdateScore(0);
         UpdateLives(3);
         titleScreen.gameObject.SetActive(false);
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false ;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
